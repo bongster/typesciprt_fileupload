e0f70ebe5d652e36060791b90ecb5de1ea90b644
Daniel <bongster88@gmail.com>
2019-12-18 17:23:19 +0800

    add register validation and warning

--- src/public/scripts/register.js
+++ src/public/scripts/register.js
@@ -1,5 +1,5 @@
 /******************************************************************************
- *                        Add, Edit, and Delete Users
+ *                        Document Events
  ******************************************************************************/
 
 document.addEventListener('click', function (event) {
@@ -17,26 +17,62 @@ document.addEventListener('change', function (event) {
 })
 
 
-function registerUser() {
-  var firstNameInput = document.getElementById('first-name-input');
-  var lastNameInput = document.getElementById('last-name-input');
-  var NameInput = document.getElementById('user-name-input');
-  var pwdInput = document.getElementById('pwd-input');
-  var pwdConfirmInput = document.getElementById('pwd-confirm-input');
+/******************************************************************************
+ * Callback Function
+ ******************************************************************************/
 
-  var data = {
-    user: {
-      name: NameInput.value,
-      first_name: firstNameInput.value,
-      last_name: lastNameInput.value,
-      password: pwdInput.value,
-      password_confirm: pwdConfirmInput.value,
-    },
-  };
-  Http.Post('/api/register', data)
-    .then(() => {
-      window.location.href = '/';
+function showError(errors) {
+  resetError();
+  let EL, errMsgEL, id;
+  Object.keys(errors).map(key => {
+    id = DataIdMap[key];
+    const EL = document.getElementById(id);
+    EL.parentElement.classList.add('danger');
+    errMsgEL = document.getElementById(`${id}-error-message`)
+    if (errMsgEL) {
+      errMsgEL.innerText = errors[key];
+    }
+  });
+}
+
+function resetError() {
+  Object.keys(DataIdMap).map(key => {
+    document.getElementById(DataIdMap[key]).parentElement.classList.remove('danger');
+  });
+}
+
+/******************************************************************************
+ * API
+ ******************************************************************************/
+
+const DataIdMap = {
+  name: 'user-name-input',
+  first_name: 'first-name-input',
+  last_name: 'last-name-input',
+  password: 'pwd-input',
+  password_confirm: 'pwd-confirm-input',
+}
+
+function getData() {
+  return Object.keys(DataIdMap).reduce((pre, key) => {
+    pre[key] = document.getElementById(DataIdMap[key]).value;
+    return pre;
+  }, {});
+}
+
+function registerUser() {
+  var data = getData();
+
+  Validate(data).then(validatedData => {
+    Http.Post('/api/register', {
+      user: validatedData,
     })
+      .then(() => {
+        window.location.href = '/';
+      })
+  }, errors => {
+    showError(errors);
+  })
 }
 
 
@@ -44,32 +80,27 @@ function registerUser() {
  *                        Validation
  ******************************************************************************/
 
-function required() {
-  var NameInput = document.getElementById('user-name-input');
-  var pwdInput = document.getElementById('pwd-input');
-  var pwdConfirmInput = document.getElementById('pwd-confirm-input');
-  return (NameInput.value && pwdInput.value && pwdConfirmInput.value);
+function required(d) {
+  const data = d || getData();
+  return (data.name && data.password && data.password_confirm);
 }
 
-function validation() {
+function Validate(values) {
   const errors = {};
-  var pwdInput = document.getElementById('pwd-input');
-  var pwdConfirmInput = document.getElementById('pwd-confirm-input');
-  
-  if (pwdInput.value !== pwdConfirmInput.value) {
-    errors['pwd-confirm-input'] = 'Passwords do not match, please try again';
-  }
+  return new Promise((resolve, reject) => {
+    if (values.password !== values.password_confirm) {
+      errors.password_confirm = 'Passwords do not match, please try again';
+    }
 
-  return errors;
+    if (values.password.length < 6 || values.password.length > 20) {
+      errors.password = 'Please enter 6-20 alpha-numeric or symbol characters';
+    }
+
+    if (Object.keys(errors).length) {
+      return reject(errors);
+    } else {
+      return resolve(values);
+    }
+  })
 }
 
-/******************************************************************************
- *                        Add, Edit, and Delete Users
- ******************************************************************************/
-
-function logoutUser() {
-  Http.Get('/api/auth/logout')
-    .then(() => {
-      window.location.href = '/';
-    })
-}
